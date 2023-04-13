4afe2927bb880b5ca139d463342db16b96908060
Lucca Malta de Freitas <luccamaltafreitas@gmail.com>
2023-04-13 17:55:38 -0300

    Javascript from index

---
+++ assets/js/JS.js
@@ -0,0 +1,27 @@
+var downloadBtn = document.getElementById("downloadBtn");
+downloadBtn.addEventListener("click", function() {
+    window.location = "/assets/docs/Curriculo.pdf";
+});
+
+var typed = new Typed (".typedText", {
+    strings: ["desenvolvedor Front-End."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true
+})
+
+//menu-sectionn responsivo
+
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = document.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
